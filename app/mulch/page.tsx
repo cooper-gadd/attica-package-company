cb1ef21267bc129035d6ae0232c51ab45de9e37f
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2025-03-06 20:23:04 -0500

    mulch page

--- app/mulch/page.tsx
+++ app/mulch/page.tsx
@@ -2,12 +2,12 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: "Playground",
+  title: "Mulch",
   description:
-    "We offer Certified Playground Material that is safe for your kids to play on.",
+    "We offer both natural brown and dyed black mulch for your garden.",
 };
 
-export default function PlaygroundPage() {
+export default function MulchPage() {
   return (
     <main>
       <div className="border-grid border-b">
@@ -16,12 +16,16 @@ export default function PlaygroundPage() {
             <div className="container">
               <div className="max-w-3xl mx-auto text-center">
                 <h1 className="text-4xl font-bold mb-4">
-                  Material For Your Playground
+                  Mulch For Your Garden
                 </h1>
                 <p className="text-lg text-muted-foreground mb-6">
-                  We offer Certified Playground Material that is safe for your
-                  kids to play on. We have documentation that is available upon
-                  request to support our Certified Playground Material.
+                  We offer both natural brown and dyed black mulch for your
+                  garden. The natural brown mulch is made from a blend of
+                  hardwoods and is quad ground to a fine texture. The dyed black
+                  mulch is produced from an identical blend of hardwoods. It
+                  undergoes a triple grinding process before being colored with
+                  a black colorant. Both are available for delivery or pick-up
+                  at our facility.
                 </p>
               </div>
             </div>
@@ -36,24 +40,22 @@ export default function PlaygroundPage() {
               <div className="flex flex-col lg:flex-row items-center gap-12">
                 <div className="w-full lg:w-1/2 text-center md:text-start">
                   <h2 className="text-3xl font-semibold mb-6">
-                    How It&apos;s Made
+                    How Natural Mulch is Made
                   </h2>
                   <div className="space-y-6">
                     <p className="text-lg text-muted-foreground leading-relaxed">
-                      The Certified Playground Material is manufactured from
-                      clean chips sent through our 2755 Morbark Chipper. The
-                      wood chips are then processed through our Morbark 6400 XT
-                      Horizontal Grinder to obtain the desired specification.
-                      The material is then sent thru our Star Screener to remove
-                      sawdust.
+                      The Natural Brown Mulch is manufactured by using raw bark
+                      through a Morbark 6400 XT Horizontal Grinder. Once the
+                      material is at the desired specification it is sent
+                      through our Star Screener to remove fines and overs.
                     </p>
                   </div>
                 </div>
                 <div className="w-full lg:w-1/2">
                   <div className="aspect-[4/3] relative overflow-hidden rounded-lg">
                     <Image
-                      src="/playground-home.jpg"
-                      alt="Playground Home"
+                      src="/mulch-brown-house-corner.jpg"
+                      alt="Mulch Brown House Corner"
                       width={800}
                       height={600}
                       className="object-cover w-full h-full"
@@ -73,22 +75,25 @@ export default function PlaygroundPage() {
               <div className="flex flex-col lg:flex-row items-center gap-12">
                 <div className="w-full lg:w-1/2 text-center md:text-start">
                   <h2 className="text-3xl font-semibold mb-6">
-                    How It&apos;s Tested
+                    How is Dyed Mulch Made?
                   </h2>
                   <div className="space-y-6">
                     <p className="text-lg text-muted-foreground leading-relaxed">
-                      The Engineered Wood Fiber has been shipped out to be
-                      tested for size, consistency, cushion, compaction and
-                      wheelchair accessibility to be safely distributed at
-                      parks, schools, municipalities and residential homes.
+                      The Dyed Black Mulch is manufactured from a process of
+                      grinding slash through a Morbark 6400 XT Horizontal
+                      Grinder. Once the material is at the desired specification
+                      it is sent through our Star Screener to remove fines and
+                      overs. Finally, the mulch is distributed into the
+                      Doppstadt Colorizer to add the dyed black colorant to the
+                      material.
                     </p>
                   </div>
                 </div>
                 <div className="w-full lg:w-1/2">
                   <div className="aspect-[4/3] relative overflow-hidden rounded-lg">
                     <Image
-                      src="/playground-school.jpg"
-                      alt="Playground School"
+                      src="/mulch-black-house-gate.jpg"
+                      alt="Mulch Black House Gate"
                       width={800}
                       height={600}
                       className="object-cover w-full h-full"
